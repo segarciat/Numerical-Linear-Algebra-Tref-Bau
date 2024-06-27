Fix vector angle calculation bug
Before, the angle_from() function was using the vector [0, 1]
to calculate the angle between a vector v and the x-axis. This
was supposed to be [1, 0]. This is the reason why passing vector
u2 was yielding the correct result. I fixed it to pass u1.
Moreover, instead of defaulting to [1, 0], I defaulted to
a vector e1, the first standard basis vector.

I had missed the part of the instruction that said to apply the
program to the matrices of exercise 1, so I added a loop to plot
each of these. Further, I added a command-line option to allow
the user to enter an image path, causing the progrma to additionally
save an image for each of the plot figures with that name pattern.

04-The-Singular-Value-Decomposition/exercises/03-svd-vector-plot/main.py
@@ -1,10 +1,19 @@
 from plot_singular_vectors import svd_and_plot
 import matplotlib
 import numpy as np
+import sys
+import argparse
 
-if __name__ == '__main__':	
-	# See: https://matplotlib.org/stable/users/explain/figure/backends.html#what-is-a-backend
-	# Changes from the default non-interactive Agg backend, since that is useful only to write to files
-	matplotlib.use('qtagg')
-	A = np.array([[1, 2], [0, 2]])
-	svd_and_plot(A)
+if __name__ == '__main__':
+
+	matrices = [
+		np.array([[1, 2], [0,  2]]),			# Equation 3.7, Example 3.1
+		np.array([[3, 0], [0, -2]]),			# 4.1a
+		np.array([[2, 0], [0,  3]]),			# 4.1b
+		np.array([[0, 2], [0,  0], [0, 0]]),	# 4.1c
+		np.array([[1, 1], [0,  0]]),			# 4.1d
+		np.array([[1, 1], [1,  1]]),			# 4.1e
+	]
+	for A, prefix in zip(matrices, ['eq37', 'a', 'b', 'c', 'd', 'e']):
+		figure_save_path = f'{prefix}_{sys.argv[1]}' if len(sys.argv) > 1 else None
+		svd_and_plot(A, figure_save_path=figure_save_path)

04-The-Singular-Value-Decomposition/exercises/03-svd-vector-plot/plot_singular_vectors.py
@@ -8,7 +8,8 @@ import matplotlib.patches
 def angle_from(v: np.array, u: np.array=None) -> float:
 	""" Computes angle between v and u. If u is not given, [0, 1] is assumed (angle from x-axis) """
 	if u is None:
-		u = np.array([0, 1])
+		u = np.zeros(v.shape)
+		u[0] = 1
 	v_norm = linalg.norm(v)
 	u_norm = linalg.norm(u)
 	return np.arccos(np.clip(np.dot(v / v_norm, u / u_norm), -1, 1)) # Clip to avoid rounding issues.
@@ -40,12 +41,11 @@ def plot_left_singular_vectors(U: np.array, s: np.array, ax: matplotlib.axes.Axe
 	# Extract and scale left singular vectors
 	sigma1, sigma2 = s
 	u1, u2 = U[:, 0], U[:, 1]
-	u1_scaled = u1 * sigma1
-	u2_scaled = u2 * sigma2
 	
 	# Plot ellipse
+	sigma2 = sigma2 if sigma2 > 0 else 1
 	origin = np.array([0, 0])
-	rotation_angle = angle_from(u2_scaled) * (180 / math.pi) # in degrees
+	rotation_angle = np.sign(u1[1]) * angle_from(u1) * (180 / math.pi) # in degrees
 	ax.axis('equal')
 	ellipse = matplotlib.patches.Ellipse(
 		xy=origin, width=2*sigma1, height=2*sigma2, angle=rotation_angle, fill=False
@@ -53,6 +53,8 @@ def plot_left_singular_vectors(U: np.array, s: np.array, ax: matplotlib.axes.Axe
 	ax.add_patch(ellipse)
 
 	# Plot left singular vectors
+	u1_scaled = u1 * sigma1
+	u2_scaled = u2 * sigma2
 	ax.quiver(0, 0, u1_scaled[0], u1_scaled[1], angles='xy', scale_units='xy',
 			scale=1, color=['r'], label='$\sigma_1 \cdot u_1$')
 	ax.quiver(0, 0, u2_scaled[0], u2_scaled[1], angles='xy', scale_units='xy',
@@ -61,14 +63,16 @@ def plot_left_singular_vectors(U: np.array, s: np.array, ax: matplotlib.axes.Axe
 	ax.set_title("Left singular vectors")
 
 
-def svd_and_plot(A : np.array, figname=None) -> None:
+def svd_and_plot(A : np.array, figure_save_path=None) -> None:
 	"""
-		Given matrix A, computes the SVD and plots the left and right singular vectors.
+		Given matrix 2-column matrix A, computes the SVD and plots the left and right singular vectors.
 		If figname is given, saves a file with that name. Otherwise displays the figure.
 	"""
 	U, s, Vt = linalg.svd(A)
 	fig, (ax1, ax2) = plt.subplots(1, 2)
 	plot_right_singular_vectors(Vt, ax1)
 	plot_left_singular_vectors(U, s, ax2)
-	fig.suptitle("Image of unit sphere under linear map")
+	fig.suptitle(f"Image of unit sphere under linear map, $\sigma_1={np.round(s[0], 4)}$, $\sigma_2={np.round(s[1], 4)}$")
+	if figure_save_path:
+		plt.savefig(figure_save_path)
 	plt.show()